87d4487534e6c9bbbc8c8f10f539b50760ea092b
Totalzone <totalzone.contact@gmail.com>
2025-06-06 10:37:14 +0200

    prima visione

--- src/modules/home/components/featured-products/index.tsx
+++ src/modules/home/components/featured-products/index.tsx
@@ -1,16 +1,47 @@
+import { listProducts } from "@lib/data/products"
 import { HttpTypes } from "@medusajs/types"
-import ProductRail from "@modules/home/components/featured-products/product-rail"
+import { Text } from "@medusajs/ui"
 
-export default async function FeaturedProducts({
-  collections,
+import InteractiveLink from "@modules/common/components/interactive-link"
+import ProductPreview from "@modules/products/components/product-preview"
+
+export default async function ProductRail({
+  collection,
   region,
 }: {
-  collections: HttpTypes.StoreCollection[]
+  collection: HttpTypes.StoreCollection
   region: HttpTypes.StoreRegion
 }) {
-  return collections.map((collection) => (
-    <li key={collection.id}>
-      <ProductRail collection={collection} region={region} />
-    </li>
-  ))
+  const {
+    response: { products: pricedProducts },
+  } = await listProducts({
+    regionId: region.id,
+    queryParams: {
+      collection_id: collection.id,
+      fields: "*variants.calculated_price",
+    },
+  })
+
+  if (!pricedProducts) {
+    return null
+  }
+
+  return (
+    <div className="content-container py-12 small:py-24">
+      <div className="flex justify-between mb-8">
+        <Text className="txt-xlarge">{collection.title}</Text>
+        <InteractiveLink href={`/collections/${collection.handle}`}>
+          View all
+        </InteractiveLink>
+      </div>
+      <ul className="grid grid-cols-2 small:grid-cols-3 gap-x-6 gap-y-24 small:gap-y-36">
+        {pricedProducts &&
+          pricedProducts.map((product) => (
+            <li key={product.id}>
+              <ProductPreview product={product} region={region} isFeatured />
+            </li>
+          ))}
+      </ul>
+    </div>
+  )
 }
--- src/modules/products/templates/index.tsx
+++ src/modules/products/templates/index.tsx
@@ -1,69 +1,40 @@
-import React, { Suspense } from "react"
-
-import ImageGallery from "@modules/products/components/image-gallery"
-import ProductActions from "@modules/products/components/product-actions"
-import ProductOnboardingCta from "@modules/products/components/product-onboarding-cta"
-import ProductTabs from "@modules/products/components/product-tabs"
-import RelatedProducts from "@modules/products/components/related-products"
-import ProductInfo from "@modules/products/templates/product-info"
-import SkeletonRelatedProducts from "@modules/skeletons/templates/skeleton-related-products"
-import { notFound } from "next/navigation"
-import ProductActionsWrapper from "./product-actions-wrapper"
 import { HttpTypes } from "@medusajs/types"
+import { Heading, Text } from "@medusajs/ui"
+import LocalizedClientLink from "@modules/common/components/localized-client-link"
 
-type ProductTemplateProps = {
+type ProductInfoProps = {
   product: HttpTypes.StoreProduct
-  region: HttpTypes.StoreRegion
-  countryCode: string
 }
 
-const ProductTemplate: React.FC<ProductTemplateProps> = ({
-  product,
-  region,
-  countryCode,
-}) => {
-  if (!product || !product.id) {
-    return notFound()
-  }
-
+const ProductInfo = ({ product }: ProductInfoProps) => {
   return (
-    <>
-      <div
-        className="content-container flex flex-col small:flex-row small:items-start py-6 relative"
-        data-testid="product-container"
-      >
-        <div className="flex flex-col small:sticky small:top-48 small:py-0 small:max-w-[300px] w-full py-8 gap-y-6">
-          <ProductInfo product={product} />
-          <ProductTabs product={product} />
-        </div>
-        <div className="block w-full relative">
-          <ImageGallery images={product?.images || []} />
-        </div>
-        <div className="flex flex-col small:sticky small:top-48 small:py-0 small:max-w-[300px] w-full py-8 gap-y-12">
-          <ProductOnboardingCta />
-          <Suspense
-            fallback={
-              <ProductActions
-                disabled={true}
-                product={product}
-                region={region}
-              />
-            }
+    <div id="product-info">
+      <div className="flex flex-col gap-y-4 lg:max-w-[500px] mx-auto">
+        {product.collection && (
+          <LocalizedClientLink
+            href={`/collections/${product.collection.handle}`}
+            className="text-medium text-ui-fg-muted hover:text-ui-fg-subtle"
           >
-            <ProductActionsWrapper id={product.id} region={region} />
-          </Suspense>
-        </div>
+            {product.collection.title}
+          </LocalizedClientLink>
+        )}
+        <Heading
+          level="h2"
+          className="text-3xl leading-10 text-ui-fg-base"
+          data-testid="product-title"
+        >
+          {product.title}
+        </Heading>
+
+        <Text
+          className="text-medium text-ui-fg-subtle whitespace-pre-line"
+          data-testid="product-description"
+        >
+          {product.description}
+        </Text>
       </div>
-      <div
-        className="content-container my-16 small:my-32"
-        data-testid="related-products-container"
-      >
-        <Suspense fallback={<SkeletonRelatedProducts />}>
-          <RelatedProducts product={product} countryCode={countryCode} />
-        </Suspense>
-      </div>
-    </>
+    </div>
   )
 }
 
-export default ProductTemplate
+export default ProductInfo
