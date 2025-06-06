54e5e7c3af817281186c8eb66eaef211ea4dab4b
Totalzone <totalzone.contact@gmail.com>
2025-06-06 09:08:25 +0200

    Aggiornato progetto React TotalZone corretto

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,10 @@
-// Contenuto fittizio di main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div style={{ fontSize: "24px", padding: "20px" }}>
+      Benvenuto nel sito React TotalZone con registrazione!
+    </div>
+  </React.StrictMode>
+)
