191df5917cf4ccd226f015e102cf6203247792f9
Totalzone <totalzone.contact@gmail.com>
2025-06-09 20:34:23 +0200

    prima visione sito

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ fontFamily: "Arial", textAlign: "center", marginTop: "50px" }}>
-      <h1>Benvenuto su TotalZone</h1>
-      <p>Questo è un sito Next.js pronto per Netlify ✅</p>
-    </div>
-  );
-}
